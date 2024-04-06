d966a9c507ffe498d26efaa6dbeb499dc5a01c91
Safahadidy <safahadidy@gmail.com>
2024-04-06 13:49:11 +0200

    c

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""Module to query the Reddit API and returns the number of subscribers"""
+"""queries the Reddit API and prints the titles of the first 10 hot
+    posts listed for a given subreddit."""
 import requests
 
 
 def top_ten(subreddit):
-    """Returns the number of subscribers"""
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        for i in range(10):
-            title = response.json()["data"]["children"][i]["data"]["title"]
-            print(title)
+    """querie Reddit API and prints first 10 hot posts"""
+
+    headers = {'User-Agent': 'MyPythonScript/1.0'}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    request = requests.get(url=url, headers=headers, allow_redirects=False)
+    if request.status_code == 200:
+        hot_ten = request.json().get('data').get('children')
+        if not hot_ten:
+            return print("None")
+        else:
+            for child in hot_ten:
+                print(child.get('data').get("title"))
     else:
-        print(None)
+        return print("None")
